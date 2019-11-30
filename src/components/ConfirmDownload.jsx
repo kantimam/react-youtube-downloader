fb185b58e298c7ee7d10b6e1acc659c2f2fe7f9b
Nyannazz <kantemir.imam@gmail.com>
2019-11-30 23:51:42 +0100

    confirm name / metadata component added

---
+++ src/components/ConfirmDownload.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ConfirmDownload = () => {
+    return (
+        <div id="confirmContainer centerAll">
+            <form action="">
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+            </form>
+        </div>
+    )
+}

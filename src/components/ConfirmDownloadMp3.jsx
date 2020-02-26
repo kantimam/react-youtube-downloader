2c7001a74b41d03fd713d34c2f38e64dd17f5811
Nyannazz <kantemir.imam@gmail.com>
2020-02-26 11:17:51 +0100

    lazy loading added and fixes in confirm components

--- src/components/ConfirmDownloadMp3.jsx
+++ src/components/ConfirmDownloadMp3.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { downloadVideo, getFileSize } from '../api/api';
+import { downloadMp3, getFileSize } from '../api/api';
 import CloseIcon from './CloseIcon';
 import Progress from './Progress';
 
-const ConfirmDownload = ({ match, video, history }) => {
+const ConfirmDownloadMp3 = ({ match, video, history }) => {
     const [downloadState, setDownload] = useState("download");
     const [name, setName] = useState("");
 
@@ -11,11 +11,23 @@ const ConfirmDownload = ({ match, video, history }) => {
     const [error, setError] = useState("");
     const [progress, setProgress] = useState(0);
 
+    const [artist, setArtist] = useState("");
+    const [song, setSong] = useState("");
+
     const download = (event) => {
         event.preventDefault();
         if(downloadState==="download"){
             setDownload("loading")
-            return downloadVideo(match.params.query, match.params.itag, name, metadata, (loaded)=>setProgress(loaded), (state)=>setDownload(state))
+            const cover=video && video.thumbnail
+            return downloadMp3(
+                match.params.query, 
+                match.params.itag, 
+                name,
+                metadata, 
+                {title: song, artist: artist, cover: cover}, 
+                (loaded)=>setProgress(loaded), 
+                (state)=>setDownload(state)
+            );
         }
         if(downloadState==="failed :("){
             return reset();
@@ -23,7 +35,6 @@ const ConfirmDownload = ({ match, video, history }) => {
         if(downloadState==="finished"){
             return close();
         }
-
     }
     const reset=()=>{
         setMeta(null);
@@ -32,20 +43,33 @@ const ConfirmDownload = ({ match, video, history }) => {
         setDownload("download");
     }
 
-    const close=()=>{
-        history.push(`/video/${match.params.query}`)
-    }
-
     useEffect(() => {
-        reset()
+        reset();
         if (video) setName(video.title)
         getFileSize(match.params.query, match.params.itag).then(response => {
-            setMeta(response.data);
+            setMeta(response.data)
         }).catch(e => {
             console.log(e);
             setError("failed to get videoinfo... you can try download anyway")
         });
     }, [video, match.params.query, match.params.itag])
+
+
+    const generateMp3Tags=(event)=>{
+        event.preventDefault();
+        if(video.title){
+            const parts=video.title.split("-");
+            if(parts.length===2){
+                setArtist(parts[0].trim());
+                setSong(parts[1].trim());
+            }
+        }
+    }
+
+    const close=()=>{
+        history.push(`/video/${match.params.query}`)
+    }
+
 
     if (!video) return <div id="confirmModal" className="centerAll"><h1>LOADING</h1></div>
 
@@ -56,11 +80,15 @@ const ConfirmDownload = ({ match, video, history }) => {
                 <h1 className="centerText">CONFIRM DOWNLOAD</h1>
                 {metadata && <Progress size={metadata.size} progress={progress} />}
                 <input value={name} onChange={(e) => setName(e.target.value)} type="text" />
+                
+                <button onClick={generateMp3Tags} id="autoMp3Button" className="marginAuto">AUTO GENERATE</button>
+                
+                <input value={artist} placeholder="add an artist if you want" onChange={(e) => setArtist(e.target.value)} type="text" />
+                <input value={song} placeholder="add song name" onChange={(e) => setSong(e.target.value)} type="text" />
                 <input className="submit hoverShadow" value={downloadState} type="submit" />
             </form>
         </div>
     )
 }
 
-
-export default ConfirmDownload
+export default ConfirmDownloadMp3

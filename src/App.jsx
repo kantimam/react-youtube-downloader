d63b9ea58ad64b8ac24f678c29cdc5dd24fac5eb
Nyannazz <kantemir.imam@gmail.com>
2020-02-26 12:29:30 +0100

    share working deploy

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,9 @@ function App() {
             <Switch>
               {/* route to share youtube urls from other android apps (youtube for example <3) only works on adroid for now :(  */}
               <Route path="/share" render={({ history }) => redirectIfValid(history)} />
+              
+              {/* debug share function */}
+              {/* <Route path="/share" render={({ history }) => <div className="centerAll" id="debugFullHeight">{history.location.search.toString()}</div>} /> */}
 
               <Route path="/search" component={YoutubeSearch} />
               <Route path="/" render={({ history }) => <Downloader history={history} video={video} setVideo={setVideo} />} />
